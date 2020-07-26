f3e75dd848f69f516a4061a81335c787def677a5
Glauco Elias Margarido <glauco.margarido@gmail.com>
2020-07-26 19:47:42 -0300

    Adiciona campo ao usuário (#4)

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_195505) do
+ActiveRecord::Schema.define(version: 2020_07_26_220903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
